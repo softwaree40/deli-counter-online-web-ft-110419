2fb437bc73d2ef3ea26f64e693c1af7addec5d1a
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-05 22:26:14 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,14 +14,26 @@ def line(katz_deli)
     #binding.pry
     end
      puts text 
+     #binding.pry
   end
   
 end
 def take_a_number(katz_deli, name)
    katz_deli << name
-   puts "Welcome, #{name}. You are number 1 in line."
+   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
     #binding.pry
     
 end
+
+def now_serving(katz_deli)
+    if katz_deli.size == 0 
+   puts "There is nobody waiting to be served!"
+   else
+      puts "Currently serving #{katz_deli.first}."
+      katz_deli.shift
+    #binding.pry
+   end
+end
+
 
 
